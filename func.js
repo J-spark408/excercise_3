922b027233bf4efa72daa909785fb047858afcbb
John <Sungwook_park@csu.fullerton.edu>
2023-09-29 20:07:58 -0700

    adjusting the func.js

--- func.js
+++ func.js
@@ -1,6 +1,7 @@
 var value = document.querySelector("column-btn");
 var clear = document.getElementById("clear");
 var input = document.getElementById("txt-input");
+var operatorBox = document.getElementById("operator-output");
 var calculated = false;
 
 // document.querySelectorAll("button").forEach(button => {
@@ -12,47 +13,66 @@ for (let button of buttons) {
     button.addEventListener("click", display)
 }
 
+var firstVal = 0;
+var secondVal = 0;
+var operator;
+var setOperator = false;
 
 function display(e) {
     input.value += e.target.innerHTML;
     if (calculated) {
         clearScreen();
     }
-
-    if (input.value.charAt(0) == 0) {
-        input.value = e.target.innerHTML;
-    }
-    if ('+-*/'.includes(input.value.charAt(0))) {
+    if ('+-*/'.includes(e.target.innerHTML) && firstVal == 0 && calculated == false) {
+        getValue(input.value.slice(0, -1, ""));
+        getOperator(e.target.innerHTML);
         input.value = "";
     }
-    const lastChar = input.value[input.value.length - 2];
-    if ('+-*/'.includes(lastChar) && '+-*/'.includes(e.target.innerHTML)) {
-        input.value = input.value.slice(0, -2) + e.target.innerHTML;
-    }
-    if (e.target.innerHTML == '=') {
-        input.value = input.value.slice(0, -1) + "";
-        if ('+-*/'.includes(lastChar)) {
-            input.value = "Invalid input";
-            calculated = true;
-            return;
-        } else {
-            calculate(input.value);
-        }
+    if ('='.includes(e.target.innerHTML) && firstVal != 0 &&
+        secondVal == 0 && calculated == false && input.value.length > 0) {
+        getValue(input.value.slice(0, -1, ""));
+        input.value = "";
+        calculate(firstVal, secondVal, operator);
+    } else {
+        return;
     }
 }
 
-function calculate(val) {
-    try {
-        const result = eval(val);
-        input.value = result;
-        calculated = true;
-    } catch (error) {
-        input.value = "Invalid Input"
+function getOperator(oper) {
+    operator = oper;
+    operatorBox.value = operator;
+    setOperator = true;
+}
+
+function getValue(val) {
+    if (firstVal == 0 && setOperator == false) {
+        firstVal = val;
     }
+    if (secondVal == 0 && setOperator == true) {
+        secondVal = val;
+    }
+}
+
+function calculate(firstVal, secondVal, operator) {
+    if (operator == '+') {
+        input.value = parseFloat(firstVal) + parseFloat(secondVal);
+    } else if (operator == '-') {
+        input.value = firstVal - secondVal;
+    } else if (operator == '*') {
+        input.value = firstVal * secondVal;
+    } else if (operator == '/') {
+        input.value = firstVal / secondVal;
+    }
+    calculated = true;
+    return input.value;
 }
 
 clear.addEventListener("click", clearScreen);
 function clearScreen() {
     input.value = "";
+    operatorBox.value = "";
+    firstVal = 0;
+    secondVal = 0;
+    setOperator = false;
     calculated = false;
 }
